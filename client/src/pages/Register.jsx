73aa1e84e3b154dd7a66e25420a56c87002eff9e
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-06-15 23:56:21 +0530

    feat: enhance Register component with form inputs and styling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <>
-    <div>Register</div>
-    </>
-  )
-}
+    <div className="text-center mt-10">
+      <h1 className="text-2xl font-bold">Register</h1>
 
-export default Register
+      <input
+        type="text"
+        placeholder="Name"
+        className="border p-2 m-2"
+      />
+
+      <br />
+
+      <input
+        type="email"
+        placeholder="Email"
+        className="border p-2 m-2"
+      />
+
+      <br />
+
+      <button className="bg-red-500 text-white px-4 py-2 rounded">
+        Register
+      </button>
+    </div>
+  );
+};
+
+export default Register;
