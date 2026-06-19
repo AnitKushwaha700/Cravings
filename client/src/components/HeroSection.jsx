5840e30f6d85c4bd29fed43a5d822cc1ee0cc973
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-06-19 21:01:51 +0530

    feat: enhance HeroSection component with improved carousel layout and accessibility

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -6,43 +6,67 @@ import img4 from "../assets/bgImage4-L1QELaMd.jpg";
 
 const HeroSection = () => {
   return (
-    <div className="carousel w-full ">
-      <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} alt="" className="w-full h-96 object-cover" />
+    <>
+      <div className="carousel w-full h-[500px]">
 
-        <div className="absolute left-5 right-5 top-1/2 flex justify-between">
-          <a href="#slide3" className="btn btn-circle">❮</a>
-          <a href="#slide2" className="btn btn-circle">❯</a>
+        {/* Slide 1 */}
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} alt="Slide 1" className="w-full object-cover" />
+
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 justify-between">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div id="slide2" className="carousel-item relative w-full">
-        <img src={img2} alt="" className="w-full h-96 object-cover" />
+        {/* Slide 2 */}
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} alt="Slide 2" className="w-full object-cover" />
 
-        <div className="absolute left-5 right-5 top-1/2 flex justify-between">
-          <a href="#slide1" className="btn btn-circle">❮</a>
-          <a href="#slide3" className="btn btn-circle">❯</a>
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 justify-between">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div id="slide3" className="carousel-item relative w-full">
-        <img src={img3} alt="" className="w-full h-96 object-cover" />
+        {/* Slide 3 */}
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} alt="Slide 3" className="w-full object-cover" />
 
-        <div className="absolute left-5 right-5 top-1/2 flex justify-between">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 justify-between">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div id="slide4" className="carousel-item relative w-full">
-        <img src={img4} alt="" className="w-full h-96 object-cover" />
+        {/* Slide 4 */}
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} alt="Slide 4" className="w-full object-cover" />
 
-        <div className="absolute left-5 right-5 top-1/2 flex justify-between">
-          <a href="#slide2" className="btn btn-circle">❮</a>
-          <a href="#slide1" className="btn btn-circle">❯</a>
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 justify-between">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
+
       </div>
-    </div>
+    </>
   );
 };
 
