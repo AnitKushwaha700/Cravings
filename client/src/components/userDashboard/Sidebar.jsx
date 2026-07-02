806941e9edfbb7ecb62827f8accd0f570bd0bf99
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-07-02 21:06:31 +0530

    feat: enhance UserDashboard and Sidebar components; implement dynamic menu items and layout adjustments

--- client/src/components/userDashboard/Sidebar.jsx
+++ client/src/components/userDashboard/Sidebar.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineDashboard, MdOutlineFastfood } from "react-icons/md";
+import { FaFolderOpen } from "react-icons/fa";
+import { BsCalendar2HeartFill } from "react-icons/bs";
+import { TbSettingsBolt } from "react-icons/tb";
 
-const sidebar = () => {
+const MenuItems = [
+  { name: "Overview", icon: <MdOutlineDashboard /> },
+  { name: "Orders", icon: <MdOutlineFastfood /> },
+  { name: "Wishlist", icon: <BsCalendar2HeartFill /> },
+  { name: "Settings", icon: <TbSettingsBolt /> },
+];
+
+const sidebar = ({ active, setActive }) => {
   return (
     <>
-    <div>sidebar</div>
+      <div className="p-3">
+        <div className="text-cnenter text-xl">
+          <div className="border-b-2 p-2 text-center font-bold ">
+            User Dashboard
+          </div>
+          <div className="space-y-3 p-2 mt-4 ">
+            {MenuItems.map((item, idx) => (
+              <button
+                key={idx}
+                className={`flex gap-3 font-semibold p-2 items-center border-2 border-(--primary) hover:border-(--primary) w-full rounded-lg ${active === item.name && "bg-(--secondary) text-(--primary-text)"}`}
+                onClick={() => setActive(item.name)}
+              >
+                {item.icon}
+                <span>{item.name}</span>
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default sidebar
+export default sidebar;
--- client/src/pages/dashboard/UserDashboard.jsx
+++ client/src/pages/dashboard/UserDashboard.jsx
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import Sidebar from "../../components/userDashboard/Sidebar.jsx";
+import Overview from "../../components/userDashboard/Overview.jsx";
+import Orders from "../../components/userDashboard/Orders.jsx";
+import Wishlist from "../../components/userDashboard/Wishlist.jsx";
+import Settings from "../../components/userDashboard/Settings.jsx";
 
 const UserDashboard = () => {
-  const { user } = useAuth();
+  const [active, setActive] = useState("Overview");
   return (
     <>
-      <div className="flex gap-3 text-2xl ">
-        <div>Sidebar</div>
-        <div>Contact</div>
+      <div className="flex h-[92vh]">
+        <div className="w-1/6 border-e-2 border-gray-500 h-full">
+          <Sidebar active={active} setActive={setActive} />
+        </div>
+        <div className="5/6 p-4 h-full">
+          {active === "Overview" && <Overview />}
+          {active === "Orders" && <Orders />}
+          {active === "Wishlist" && <Wishlist />}
+          {active === "Settings" && <Settings />}
+        </div>
       </div>
     </>
   );
