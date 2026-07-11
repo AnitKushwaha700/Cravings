e9679ed63e34ab02ad058a41e4e0f8c50527bbf7
AnitKushwaha700 <kushwahaanitak@gmail.com>
2026-07-11 19:55:01 +0530

    feat: Implement email sending functionality and user profile editing logic

--- server/src/config/email.config.js
+++ server/src/config/email.config.js
@@ -1,6 +1,42 @@
-const sendOTPEmail = (email, newOTP) => {
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const sendEmail = async (to, subject, message) => {
   try {
+    // console.log("Started Sending Email");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USERNAME,
+        pass: process.env.GMAIL_PASSCODE,
+      },
+    });
+
+    // console.log("3....2....1....");
+
+    const mailOption = {
+      from: process.env.GMAIL_USERNAME,
+      to,
+      subject,
+      html: message,
+    };
+
+    // console.log("Sending Email");
+
+    const res = await transporter.sendMail(mailOption);
+    console.log(res);
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
+
+export default sendEmail;
+
+// sendEmail(
+//   "kushwahaanitak@gmail.com",
+//   "test Email",
+//   "<p style='color:red;'> Test Message</p>",
+// );
---
+++ server/src/utils/email.service.js
@@ -0,0 +1,16 @@
+// https://res.cloudinary.com/jh3ku9oj/image/upload/v1783776924/circleLogo-DpCri5UD_goflv4.png
+
+
+import sendEmail from "../config/email.config.js"
+
+const sendOTPEmail = (email, newOTP) => {
+    try{
+        const subject = "Your OTP from Cravings"
+    }
+    catch{
+        console.log();
+        
+    }
+}
+
+
