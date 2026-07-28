f55f3163c13c3007151e0c8ceeb037be6b9517da
Anit Kushwaha <kushwahaanitak@gmail.com>
2026-07-28 20:37:48 +0530

    feat: Add RestaurantDetailsPage component with API integration for fetching restaurant details

--- client/src/pages/OrderNow.jsx
+++ client/src/pages/OrderNow.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import api from "../config/ApiConfig.js"
+import Loader from '../components/Loader.jsx'
+
+const OrderNow 
 
 const OrderNow = () => {
-  return (
+  return (  
     <>
     <div>OrderNow</div>
     </>
   )
-}
+} 
 
 export default OrderNow
